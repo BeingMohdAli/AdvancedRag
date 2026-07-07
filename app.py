e32446e1c91284762f9c3c50c4186a599d72ee9b
BeingMohdAli <mohdalisaad868@gmail.com>
2026-07-07 09:38:12 +0530

    rag

--- app.py
+++ app.py
@@ -11,3 +11,14 @@ import chromadb
 chroma_client = chromadb.Client()
 
 collection = chroma_client.get_or_create_collection(name="Advancved RAG")
+
+
+# Define text documents
+documents = [
+    {"id": "doc1", "text": "Hello, world!"},
+    {"id": "doc2", "text": "How are you today?"},
+    {"id": "doc3", "text": "Goodbye, see you later!"},
+]
+
+# Define a query text
+query = "Hello World"
